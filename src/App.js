08a31762f76e9e7e203fc3d9e3e903afd7d6654f
Jean Leconte II <jeanemileleconteii@gmail.com>
2019-05-11 23:58:30 -0400

    signin looks great

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar';
-import Dashboard from './components/dashboard/Dashboard'
+import Dashboard from './components/dashboard/Dashboard';
+import CampaignDetails from './components/campaigns/CampaignDetails';
+import SignIn from './components/auth/SignIn'
 
 class App extends Component {
   render() {
@@ -11,7 +13,9 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path='/'component={Dashboard} />
+            <Route exact path='/'component={Dashboard} />
+            <Route path='/project/:id' component={CampaignDetails} />
+            <Route path='/signin' component={SignIn} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/components/campaigns/CampaignDetails.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CampaignDetails = (props) => {
+  console.log(props);
+  const id = props.match.params.id;
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project title - { id }</span>
+          <p>Blah Blah</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by Jimmy Jam</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CampaignDetails;
