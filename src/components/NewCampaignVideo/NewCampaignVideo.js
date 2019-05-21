87a2a59a3ed26f6fd9c8a8374f06f50f67c527a1
Jean Leconte II <jeanemileleconteii@gmail.com>
2019-05-21 16:18:35 -0700

    file drop clean up

--- src/components/NewCampaignVideo/NewCampaignVideo.js
+++ src/components/NewCampaignVideo/NewCampaignVideo.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import FileDrop from 'react-file-drop';
-import { Box, Container, Heading, TextField } from 'gestalt';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { Box } from 'gestalt';
 import 'gestalt/dist/gestalt.css';
 import './react-file-drop.css'
 
-import styled from "styled-components";
-
 import OutlinedButtons from './UploadButton';
 
-const ComponentTitle = styled.div`
-    text-size: 20px;
-    max-width: 600px;
-    color: rgba(0, 0, 0, 1);
-    marginTop: 2px;
-    padding-bottom: 1px;
-    border-bottom: 1px solid rgba(0, 0, 0, 1);
-`;
+
 
 var styles = { border: '1px dashed black', height: 500, width: 325, color: "black" };
 
@@ -55,7 +43,7 @@ class NewVideoPage extends React.Component {
                         </FileDrop>
                     </div>
                 </Box>
-                
+
             </Box>
         );
     }
