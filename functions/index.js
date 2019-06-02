c61eebfd64f4f9d7e02289eb8c12a70f7a484276
Jean Leconte II <jeanemileleconteii@gmail.com>
2019-06-02 13:40:02 -0700

    decided not to risk infinite loop

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 const {Storage} = require('@google-cloud/storage');
+const gcs = require('@google-cloud/storage')();
+const os = require('os');
+const path = require('path');
 
 admin.initializeApp(functions.config().firebase);
 
@@ -12,16 +15,17 @@ admin.initializeApp(functions.config().firebase);
 async function replaceMediaNamesWithCampaignId(campaignId) {
 
   const storage = new Storage();
-  const bucketName = campaignId;
+  // const bucketName = campaignId;
 
-  const [files] = await storage.bucket(bucketName).getFiles();
+  const [files] = await storage.bucket('0wDVqgKc9EQRgwSJuWCn5Wrvt1o2/').getFiles(); // this didn't work either...
+    // look up what the name of a bucket actually is
 
   files.forEach(file => {
-    console.log(file.name);  // this didn't work because the bucket name needs to be the auth id 
+    console.log(file.name);  // this didn't work because the bucket name needs to be the auth id
   });
   console.log("cloud function ended");
 
-  /*
+  /* ... use a promise to get to the next await?
 
   // const bucketName = 'Name of a bucket, e.g. my-bucket';
   // const srcFilename = 'File to move, e.g. file.txt';
@@ -78,3 +82,51 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 
 */
+
+
+/* This should renmae all the files in the bucket if they weren't in folders...
+
+
+
+
+// object is the default bucket ... that's the peal-web-v2-appspot.com !!!!
+exports.testMethod = functions.storage.object().onChange(event => {
+
+  // step1
+  console.log(event);
+
+  // step2
+  const object = event.data;
+  const bucket = object.bucket;
+  const contentType = object.contentType;
+  const filePath = object.name;
+
+  // make sure you don't have an infinite loop
+  if (path.basename(filePath).startsWith('renamed-')) {
+    console.log("already renamed this file");
+    return;
+  }
+
+  // download
+  const destBucket = gcs.bucket(bucket);
+  const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+  const metadata = { contentType: contentType };
+  return destBucket.file(filePath).download({
+    destination: tmpFilePath
+  }).then(() => {
+    // rename
+    return destBucket.upload(tmpFilePath, {
+      destination: 'renamed-' + path.basename(filePath)
+      metadata: metadata
+    })
+  });
+});
+
+
+
+
+
+*/
+
+
+// TODO: use cloud functions to send mail chimp welcome emails
