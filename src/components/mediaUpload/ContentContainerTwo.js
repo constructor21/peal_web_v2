40bb3308b1ca98b80320994488220f51a7890bb1
Jean Leconte II <jeanemileleconteii@gmail.com>
2019-06-15 13:05:53 -0700

    form validation setup is there and you have a game plan

--- src/components/mediaUpload/ContentContainerTwo.js
+++ src/components/mediaUpload/ContentContainerTwo.js
@@ -1,13 +1,106 @@
 
 
 // https://react-dropzone.js.org/#!/Accepting%20specific%20file%20types
-
 // https://react-dropzone.netlify.com/
 
 import React, {Component, useEffect, useState} from 'react';
 import Dropzone, {useDropzone} from 'react-dropzone';
 
+const thumbsContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 16
+};
 
+const thumb = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: 'border-box'
+};
+
+const thumbInner = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+};
+
+const img = {
+  display: 'block',
+  width: 'auto',
+  height: '100%'
+};
+
+function mediaValidation() {
+  console.log("testing")
+  return true
+}
+
+function Previews(props) {
+  const [files, setFiles] = useState([]);
+    //    ^value    ^setter
+  const {getRootProps, getInputProps} = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      if(mediaValidation()) {
+        setFiles(acceptedFiles.map(file => Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        })));
+      }
+      console.log(acceptedFiles[0]) // this is what needs to be passed from child to parent and then into storage
+    }
+  });
+
+  const thumbs = files.map(file => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img
+          src={file.preview}
+          style={img}
+        />
+      </div>
+    </div>
+  ));
+
+  useEffect(() => () => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
+  return (
+    <section className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p> Click here to upload media or drag & drop! </p>
+      </div>
+      <aside style={thumbsContainer}>
+        {thumbs}
+      </aside>
+    </section>
+  );
+}
+
+class ContentContainerTwo extends Component {
+  render() {
+    return (
+      <div>
+        <Previews />
+      </div>
+    )
+  }
+}
+
+export default ContentContainerTwo
+
+
+
+/*
 class ContentContainerTwo extends Component {
   constructor() {
     super();
@@ -42,95 +135,6 @@ class ContentContainerTwo extends Component {
         )}
       </Dropzone>
     );
-  }
-}
-
-export default ContentContainerTwo
-
-
-/*
-  This works!!! 
-
-
-const thumbsContainer = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  marginTop: 16
-};
-
-const thumb = {
-  display: 'inline-flex',
-  borderRadius: 2,
-  border: '1px solid #eaeaea',
-  marginBottom: 8,
-  marginRight: 8,
-  width: 100,
-  height: 100,
-  padding: 4,
-  boxSizing: 'border-box'
-};
-
-const thumbInner = {
-  display: 'flex',
-  minWidth: 0,
-  overflow: 'hidden'
-};
-
-const img = {
-  display: 'block',
-  width: 'auto',
-  height: '100%'
-};
-
-
-function Previews(props) {
-  const [files, setFiles] = useState([]);
-  const {getRootProps, getInputProps} = useDropzone({
-    accept: 'image/*',
-    onDrop: acceptedFiles => {
-      setFiles(acceptedFiles.map(file => Object.assign(file, {
-        preview: URL.createObjectURL(file)
-      })));
-    }
-  });
-
-  const thumbs = files.map(file => (
-    <div style={thumb} key={file.name}>
-      <div style={thumbInner}>
-        <img
-          src={file.preview}
-          style={img}
-        />
-      </div>
-    </div>
-  ));
-
-  useEffect(() => () => {
-    // Make sure to revoke the data uris to avoid memory leaks
-    files.forEach(file => URL.revokeObjectURL(file.preview));
-  }, [files]);
-
-  return (
-    <section className="container">
-      <div {...getRootProps({className: 'dropzone'})}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
-      <aside style={thumbsContainer}>
-        {thumbs}
-      </aside>
-    </section>
-  );
-}
-
-class ContentContainerTwo extends Component {
-  render() {
-    return (
-      <div>
-        <Previews />
-      </div>
-    )
   }
 }
 
