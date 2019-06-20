4462b2325fbd50df9d551be5e8bb3f5fe587306e
Jean Leconte II <jeanemileleconteii@gmail.com>
2019-06-20 02:10:52 -0700

    changed to v1 fb config

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,6 +5,7 @@ import 'firebase/storage';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
+  /*
   apiKey: "AIzaSyDfWsRC1O2_uQzEgYvUuHLHMEiC-jPvHQk",
   authDomain: "peal-web-v2.firebaseapp.com",
   databaseURL: "https://peal-web-v2.firebaseio.com",
@@ -12,6 +13,14 @@ var firebaseConfig = {
   storageBucket: "peal-web-v2.appspot.com",
   messagingSenderId: "382040736163",
   appId: "1:382040736163:web:41fd5e56b2607b10"
+  */
+  apiKey: "AIzaSyD7E8rpYKYH2hHwzEBmzZSLhSxRUR1qqao",
+  authDomain: "peal-9d82d.firebaseapp.com",
+  databaseURL: "https://peal-9d82d.firebaseio.com",
+  projectId: "peal-9d82d",
+  storageBucket: "peal-9d82d.appspot.com",
+  messagingSenderId: "624902981487",
+  appId: "1:624902981487:web:cad385f22f24f8f5"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
